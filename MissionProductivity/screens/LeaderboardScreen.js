d645dbb29b0bc0e7b89daa870ec8f5e407895373
Alex Ruan <alexruan2222@gmail.com>
2022-04-03 03:11:48 -0400

    merged base templates

---
+++ MissionProductivity/screens/LeaderboardScreen.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+import { View } from "react-native";
+
+import { firebase } from "../firebase";
+
+import Scoreboard from "../components/scoreboard";
+
+const LeaderboardScreen = () => {
+  useEffect(() => {}, []);
+
+  [scores, setScores] = useState();
+
+  return (
+    <View>
+      <Scoreboard scores={scores} />
+    </View>
+  );
+};
+
+export default LeaderboardScreen;
