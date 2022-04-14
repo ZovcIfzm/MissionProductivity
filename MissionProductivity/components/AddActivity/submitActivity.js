24b410d9578f0a45b53032f0bb950b480063efcf
Alex Ruan <alexruan2222@gmail.com>
2022-04-13 23:01:04 -0400

    hook fix and firebase fix

--- MissionProductivity/components/AddActivity/submitActivity.js
+++ MissionProductivity/components/AddActivity/submitActivity.js
@@ -1,155 +1,190 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, query, onSnapshot } from "firebase/firestore";
 import db from "../../firebase.js";
-import React from 'react';
-import { Context } from "../../context.js";
+import React from "react";
 
-export async function submitActivity(name, category, ID, hours, mins, rating) {
-    const { userName } = React.useContext(Context);
-    hours = parseInt(hours);
-    mins = parseInt(mins);
-    let minutes = 0;
-    if(isNaN(mins) && isNaN(hours)) {
-        minutes =  0;
-    }
-    else if(isNaN(mins)) {
-        minutes =  (hours * 60);
-    }
-    else if(isNaN(hours)) {
-        minutes =  mins;
-    }
-    else {
-        minutes = (hours * 60) + mins;
-    }
-    
-    rating = parseInt(rating);
-    if (category === 'Both') {
-        category = "Mental & Physical Health";
-    }
-    if (rating === 0) {
-        rating = 3;
-    }
-    //Make name always first letter upper rest lower
-    name = titleCase(name);
-    //Remove all spaces from name
-    name = name.trim();
+export async function submitActivity(
+  name,
+  category,
+  ID,
+  hours,
+  mins,
+  rating,
+  userName
+) {
+  hours = parseInt(hours);
+  mins = parseInt(mins);
 
-    //Find total minutes
-    let minutes_total = minutes;
-    const mins_query = query(collection(db, "activities"), where('userId', 'in', [ID]));
-    onSnapshot(mins_query, (querySnapshot) => {
-      const results = querySnapshot.docs.map((doc) => {
-        const data = doc.data();
-        return {
-          mins: data.minutes,
-        };
-      });
-      for (let i = 0; i < results.length; i++) {
-        minutes_total += results[i];
-      }
+  let minutes = 0;
+  if (isNaN(mins) && isNaN(hours)) {
+    minutes = 0;
+  } else if (isNaN(mins)) {
+    minutes = hours * 60;
+  } else if (isNaN(hours)) {
+    minutes = mins;
+  } else {
+    minutes = hours * 60 + mins;
+  }
+
+  rating = parseInt(rating);
+  if (category === "Both") {
+    category = "Mental & Physical Health";
+  }
+  if (rating === 0) {
+    rating = 3;
+  }
+  //Make name always first letter upper rest lower
+  name = titleCase(name);
+  //Remove all spaces from name
+  name = name.trim();
+
+  //Find total minutes
+  let minutes_total = minutes;
+
+  const mins_query = query(
+    collection(db, "activities"),
+    where("userId", "in", [ID])
+  );
+  onSnapshot(mins_query, (querySnapshot) => {
+    const results = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        mins: data.minutes,
+      };
     });
+    for (let i = 0; i < results.length; i++) {
+      minutes_total += results[i];
+    }
+  });
 
-    //Find Score
-    let score_new = 0;
-    const score_query = query(collection(db, "scores"), where('userID', '==', ID));
-    onSnapshot(score_query, (querySnapshot) => {
-        const results2 = querySnapshot.docs.map((doc) => {
-          const data2 = doc.data();
-          return {
-            score: data2.score,
-          };
-        });
-        score_new = results2;
+  //Find Score
+  let score_new = 0;
+  const score_query = query(
+    collection(db, "scores"),
+    where("userID", "==", ID)
+  );
+  onSnapshot(score_query, (querySnapshot) => {
+    const results2 = querySnapshot.docs.map((doc) => {
+      const data2 = doc.data();
+      return {
+        score: data2.score,
+      };
     });
+    score_new = results2;
+  });
 
-    //Write new score
-    score_new = score_new + (0.5 * minutes_total);
+  //Write new score
+  score_new = score_new + 0.5 * minutes_total;
 
-    //unlock trophy
-    let t_100 = false;
-    let t_200 = false;
-    let t_300 = false;
-    let t_400 = false;
-    let t_500 = false;
-    let t_600 = false;
-    let t_700 = false;
-    let t_800 = false;
-    let t_900 = false;
+  //unlock trophy
+  let t_100 = false;
+  let t_200 = false;
+  let t_300 = false;
+  let t_400 = false;
+  let t_500 = false;
+  let t_600 = false;
+  let t_700 = false;
+  let t_800 = false;
+  let t_900 = false;
 
-    if (score_new > 100) {
-        t_100 = true;
-    }
-    if (score_new > 200) {
-        t_200 = true;
-    }
-    if (score_new > 300) {
-        t_300 = true;
-    }
-    if (score_new > 400) {
-        t_400 = true;
-    }
-    if (score_new > 500) {
-        t_500 = true;
-    }
-    if (score_new > 600) {
-        t_600 = true;
-    }
-    if (score_new > 700) {
-        t_700 = true;
-    }
-    if (score_new > 800) {
-        t_800 = true;
-    }
-    if (score_new > 900) {
-        t_900 = true;
-    }
-
-    await Promise.all([updateScores(score_new, userName, ID), updateTrophies(t_100, t_200, t_300, t_400, t_500, t_600, t_700, t_800, t_900, ID), updateActivity(category, minutes_total, name, rating, ID)]);
-
+  if (score_new > 100) {
+    t_100 = true;
+  }
+  if (score_new > 200) {
+    t_200 = true;
+  }
+  if (score_new > 300) {
+    t_300 = true;
+  }
+  if (score_new > 400) {
+    t_400 = true;
+  }
+  if (score_new > 500) {
+    t_500 = true;
+  }
+  if (score_new > 600) {
+    t_600 = true;
+  }
+  if (score_new > 700) {
+    t_700 = true;
+  }
+  if (score_new > 800) {
+    t_800 = true;
+  }
+  if (score_new > 900) {
+    t_900 = true;
+  }
+  await Promise.all([
+    updateScores(score_new, userName, ID),
+    updateTrophies(
+      t_100,
+      t_200,
+      t_300,
+      t_400,
+      t_500,
+      t_600,
+      t_700,
+      t_800,
+      t_900,
+      ID
+    ),
+    updateActivity(category, minutes_total, name, rating, ID),
+  ]);
 }
 
 export function checkInputs(name, category, ID, hours, mins, rating) {
-    if (((!(hours === 0)) || (!(mins === 0))) && (name) && (!(category === 'Category'))) {
-        return true;
-    }
-    return false;
+  if ((!(hours === 0) || !(mins === 0)) && name && !(category === "Category")) {
+    return true;
+  }
+  return false;
 }
 
 function titleCase(str) {
-    str = str.toLowerCase().split(' ');
-    for (var i = 0; i < str.length; i++) {
-      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-    }
-    return str.join(' ');
+  str = str.toLowerCase().split(" ");
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ");
 }
 
-async function updateScores(score, username, ID){
-    updateDoc(doc(db, "scores", ID), {
-        score: score,
-        name: username,
-        userID: ID
-    });
-  }
-  
-async function updateTrophies(t_100, t_200, t_300, t_400, t_500, t_600, t_700, t_800, t_900, ID){
-    updateDoc(doc(db, "trophies", ID), {
-        100: t_100,
-        200: t_200,
-        300: t_300,
-        400: t_400,
-        500: t_500,
-        600: t_600,
-        700: t_700,
-        800: t_800,
-        900: t_900,
-    });
-  }
-  
-async function updateActivity(category, minutes, name, rating, ID){
-    addDoc(collection(db, "activities"), {
-        category: category,
-        minutes: minutes,
-        name: name,
-        rating: rating,
-        userId: ID
-    });
-  }
+async function updateScores(score, username, ID) {
+  updateDoc(doc(db, "scores", ID), {
+    score: score,
+    name: username,
+    userID: ID,
+  });
+}
+
+async function updateTrophies(
+  t_100,
+  t_200,
+  t_300,
+  t_400,
+  t_500,
+  t_600,
+  t_700,
+  t_800,
+  t_900,
+  ID
+) {
+  updateDoc(doc(db, "trophies", ID), {
+    100: t_100,
+    200: t_200,
+    300: t_300,
+    400: t_400,
+    500: t_500,
+    600: t_600,
+    700: t_700,
+    800: t_800,
+    900: t_900,
+  });
+}
+
+async function updateActivity(category, minutes, name, rating, ID) {
+  addDoc(collection(db, "activities"), {
+    category: category,
+    minutes: minutes,
+    name: name,
+    rating: rating,
+    userId: ID,
+  });
+}
--- MissionProductivity/screens/AddActivityModal.js
+++ MissionProductivity/screens/AddActivityModal.js
@@ -1,15 +1,26 @@
 import { TabRouter } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Platform, StyleSheet, TextInput, Button, TouchableOpacity, Alert } from "react-native";
-import { AirbnbRating } from 'react-native-ratings';
+import {
+  Text,
+  View,
+  Platform,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { AirbnbRating } from "react-native-ratings";
 import { route } from "@react-navigation/native";
-import { useNavigation } from '@react-navigation/native';
-import {submitActivity, checkInputs } from '../components/AddActivity/submitActivity.js';
-import {Keyboard, TouchableWithoutFeedback} from 'react-native';
-import { Entypo } from '@expo/vector-icons'; 
+import { useNavigation } from "@react-navigation/native";
+import {
+  submitActivity,
+  checkInputs,
+} from "../components/AddActivity/submitActivity.js";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 import { Context } from "../context.js";
-import React from 'react';
-
+import React from "react";
 
 const AddActivityModal = (props) => {
   const name = props.route.params.name;
@@ -17,89 +28,102 @@ const AddActivityModal = (props) => {
   const _values = {
     hours: 0,
     mins: 0,
-    rating: 0
-  } 
+    rating: 0,
+  };
   const navigation = useNavigation();
-  const { userId } = React.useContext(Context);
+  const { userId, userName } = React.useContext(Context);
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View style={styles.container}>
-      <Text style={styles.title}>{name}</Text>
-      <Text style={styles.category}>{category}</Text>
-      <View style={styles.separator1} />
-      <View style={styles.time}>
-        <View style={styles.inputWrap}>
-            <Text style={styles.label} >Hours</Text>
-            <TextInput style={styles.inputHours}
-                 keyboardType='numeric'
-                 placeholder="0"
-                 maxLength={2}
-                 placeholderTextColor = "black"
-                 onChangeText={(val) => _values.hours = val}  
+      <View style={styles.container}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.category}>{category}</Text>
+        <View style={styles.separator1} />
+        <View style={styles.time}>
+          <View style={styles.inputWrap}>
+            <Text style={styles.label}>Hours</Text>
+            <TextInput
+              style={styles.inputHours}
+              keyboardType="numeric"
+              placeholder="0"
+              maxLength={2}
+              placeholderTextColor="black"
+              onChangeText={(val) => (_values.hours = val)}
             />
-        </View>
+          </View>
 
-        <View style={styles.inputWrap2}>
+          <View style={styles.inputWrap2}>
             <Text style={styles.label}>Minutes</Text>
-            <TextInput style={styles.inputMins}
-                 keyboardType='numeric'
-                 placeholderTextColor = "black"
-                 placeholder="0"
-                 onChangeText={(val) => _values.mins = val}  
+            <TextInput
+              style={styles.inputMins}
+              keyboardType="numeric"
+              placeholderTextColor="black"
+              placeholder="0"
+              onChangeText={(val) => (_values.mins = val)}
             />
+          </View>
         </View>
-      </View>
-      <View style={styles.separator1} />
-      <AirbnbRating
-        showRating
-        startingValue={(val) => _values.rating = val}
-        onStartRating={(val) => _values.rating = val}
-        onFinishRating={(val) => _values.rating = val}
-        selectedColor="#1CB8AE"
-        reviewColor="#1CB8AE"
-      />
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
-      <View style={styles.separator2} />
-      <TouchableOpacity
-        style={styles.addToDB}
+        <View style={styles.separator1} />
+        <AirbnbRating
+          showRating
+          startingValue={(val) => (_values.rating = val)}
+          onStartRating={(val) => (_values.rating = val)}
+          onFinishRating={(val) => (_values.rating = val)}
+          selectedColor="#1CB8AE"
+          reviewColor="#1CB8AE"
+        />
+        {/* Use a light status bar on iOS to account for the black space above the modal */}
+        <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+        <View style={styles.separator2} />
+        <TouchableOpacity
+          style={styles.addToDB}
           title="Submit Activity"
           onPress={() => {
             let valid = false;
-            valid = checkInputs(name, category, userId, _values.hours, _values.mins, _values.rating);
+            valid = checkInputs(
+              name,
+              category,
+              userId,
+              _values.hours,
+              _values.mins,
+              _values.rating
+            );
             if (valid) {
-              submitActivity(name, category, userId, _values.hours, _values.mins, _values.rating);
+              submitActivity(
+                name,
+                category,
+                userId,
+                _values.hours,
+                _values.mins,
+                _values.rating,
+                userName
+              );
               navigation.goBack(null);
-            }
-            else {
-              Alert.alert('Error Logging Activity', 'Please try again',
-              [
+            } else {
+              Alert.alert("Error Logging Activity", "Please try again", [
                 {
                   text: "Go Back",
                   onPress: () => navigation.goBack(null),
-                  style: "cancel"
+                  style: "cancel",
                 },
-                { text: "Try Again" }
+                { text: "Try Again" },
               ]);
             }
           }}
-      >
-            <Text style={styles.buttonTextSubmit}>Submit Activity</Text>
-      </TouchableOpacity>
-    </View>
+        >
+          <Text style={styles.buttonTextSubmit}>Submit Activity</Text>
+        </TouchableOpacity>
+      </View>
     </TouchableWithoutFeedback>
   );
-}
-
+};
 
 export default AddActivityModal;
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    paddingTop: 50
+    paddingTop: 50,
   },
   title: {
     fontSize: 40,
@@ -120,11 +144,11 @@ const styles = StyleSheet.create({
     height: "5%",
   },
   time: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: "row",
+    alignItems: "flex-start",
     width: "80%",
     height: 100,
-    paddingTop: "10%"
+    paddingTop: "10%",
   },
   label: {
     flex: 1,
@@ -133,43 +157,43 @@ const styles = StyleSheet.create({
   },
   inputWrap: {
     flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: "40%"
+    justifyContent: "space-between",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "40%",
   },
   inputWrap2: {
     flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    alignItems: 'center',
+    justifyContent: "space-between",
+    flexDirection: "column",
+    alignItems: "center",
     width: "40%",
-    marginLeft: "10%"
+    marginLeft: "10%",
   },
   inputHours: {
     flex: 1,
-    backgroundColor: '#D3D3D3',
-    width:"80%",
+    backgroundColor: "#D3D3D3",
+    width: "80%",
     fontSize: 20,
     textAlign: "center",
   },
   inputMins: {
     flex: 1,
-    backgroundColor: '#D3D3D3',
-    width:"80%",
+    backgroundColor: "#D3D3D3",
+    width: "80%",
     fontSize: 20,
     textAlign: "center",
   },
   addToDB: {
-    alignItems:'center',
-    justifyContent:'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: "60%",
     height: "10%",
-    backgroundColor:'#1CB8AE',
-    borderRadius:50,
+    backgroundColor: "#1CB8AE",
+    borderRadius: 50,
   },
   buttonTextSubmit: {
     color: "white",
-    fontSize:25,
-  }
+    fontSize: 25,
+  },
 });
