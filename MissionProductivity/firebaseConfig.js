9daa0810b0757ec4fa4a1a561ee6774c7b2ba4db
Alex Ruan <alexruan2222@gmail.com>
2022-05-23 14:34:41 -0400

    Revert "final edits"
    This reverts commit a5fbcca5230c153a8108c8d25c5a2f70e9b9e96a.

---
+++ MissionProductivity/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default firebaseConfig = {
+  apiKey: 'AIzaSyD0gALdf-imOqgBWkmJXKzbI0mkd9XSEWE',
+  authDomain: 'final-project-39693.firebaseapp.com',
+  projectId: 'final-project-39693',
+  storageBucket: 'final-project-39693.appspot.com',
+  messagingSenderId: '4486791808',
+  appId: '1:4486791808:web:3c0f0ef0bd4f76b7fa1c65',
+};
